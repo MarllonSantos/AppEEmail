ac51afee7bfb6cd38e837bab2b04047d78bf80d9
Marllon Santos <cmarllon.santos@gmail.com>
2024-10-26 08:29:33 -0300

    Adicionado button i, no toolbar, informando meu nome e minha matricula

--- src/assets/components/Toolbar.jsx
+++ src/assets/components/Toolbar.jsx
@@ -1,9 +1,16 @@
 function Toolbar(){
+    function direitos(){
+        alert('Criado por Carllon Santos, nº3843004402');
+    }
     return (<>
         <div id="toolbar">
-        <div className="brand">
-            <h2><i className="fa-solid fa-magnifying-glass"></i> Verifica Email</h2>
-        </div>    
+            <div className="brand">
+                <h2><i className="fa-solid fa-magnifying-glass"></i> Verifica Email</h2>
+                
+            </div>    
+            <button id="checkButton" onClick={direitos}>
+                <i class="fa-solid fa-info"></i>
+            </button>
         </div>
     </>)
 }
